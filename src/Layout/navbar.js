93b45f83fae7f2046603173ee1be2195cb741d66
elsa manang <manangelsa07@gmail.com>
2022-04-14 17:19:53 +0200

    custom navbar

--- src/Layout/navbar.js
+++ src/Layout/navbar.js
@@ -5,12 +5,14 @@ export default function Navbar() {
     return(
         <nav className="navbar">
             <div className="container">
-                <div className="row search">
-                    <div className="col-10">
-                        search
+                <div className="row">
+                    <div className="col-10 search">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <input type={'text'} className="mx-2" placeholder="Search" />
                     </div>
-                    <div className="col-2">
-                        menu
+                    <div className="col-2 justify-content-right">
+                        <span className="mx-1"><i className="fa-solid fa-bell"></i></span>
+                        <span className="mx-1 user"><i className="fa-solid fa-user"></i></span>
                     </div>
                 </div>
             </div>
