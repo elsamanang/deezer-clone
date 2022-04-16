591d9ab3fbd194c0001fa6250dfc3f4f3c6ada9b
elsa manang <manangelsa07@gmail.com>
2022-04-16 15:29:36 +0200

    first view of navigation bar

--- src/Layout/navbar.js
+++ src/Layout/navbar.js
@@ -6,13 +6,13 @@ export default function Navbar() {
         <nav className="navbar">
             <div className="container">
                 <div className="row">
-                    <div className="col-10 search">
+                    <div className="col-md-10 search">
                         <i className="fa-solid fa-magnifying-glass"></i>
                         <input type={'text'} className="mx-2" placeholder="Search" />
                     </div>
-                    <div className="col-2 justify-content-right">
-                        <span className="mx-1"><i className="fa-solid fa-bell"></i></span>
-                        <span className="mx-1 user"><i className="fa-solid fa-user"></i></span>
+                    <div className="col-md-2 nav-left">
+                        <span><i className="fa-solid fa-bell"></i></span>
+                        <span className="user"><i className="fa-solid fa-user"></i></span>
                     </div>
                 </div>
             </div>
