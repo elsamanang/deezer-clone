9d7a5df05348c68e12cc901d1cc7d371427190dc
elsa manang <manangelsa07@gmail.com>
2022-05-02 14:20:43 +0200

    add and config tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Flow from './components/flow';
 import Footer from './Layout/footer';
 import Navbar from './Layout/navbar';
 import Sidebar from './Layout/sidebar';
@@ -16,7 +17,10 @@ class App extends Component {
           </div>
           <div className='col-md-10 body'>
             <Navbar />
-            body
+            <div className='container'>
+              <Flow />
+              body
+            </div>
           </div>
         </div>
         <Footer />
