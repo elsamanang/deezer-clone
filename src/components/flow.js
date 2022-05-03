8523335d162f4480e1fdc19c28440e9b218717f6
elsa manang <manangelsa07@gmail.com>
2022-05-03 11:23:13 +0200

    add flow component

--- src/components/flow.js
+++ src/components/flow.js
@@ -5,12 +5,12 @@ export default class Flow extends Component {
 
     render() {
         return (
-            <div className="row">
+            <div className="row m-2">
                 <div className="grid">
-                    <h2>Flow, la bande-son de vos émotions</h2>
+                    <h3>Flow, la bande-son de vos émotions</h3>
                     <p>Un mix infini et personnalisé avec vos favoris et de belles découvertes</p>
                 </div>
-                <div className="grid grid-cols-7 m-2">
+                <div className="grid grid-cols-7 gap-8">
                     <FlowItem />
                     <FlowItem />
                     <FlowItem />
