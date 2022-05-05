5b0956d8188f77da42bec1d89f9c7257c6215b3b
elsa manang <manangelsa07@gmail.com>
2022-05-05 17:19:03 +0200

    recent item component

--- src/tools/recent-item.js
+++ src/tools/recent-item.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function RecentItem () {
     return (
         <div id="RecentItem">
-            <div className="grid h-56 w-56 shadow-md recent rounded-md overflow-hidden hover:opacity-75" >
+            <div className="grid h-56 w-56 shadow-md recent rounded-md overflow-hidden hover:opacity-80" >
                 <img className="rounded-full play-btn place-self-center" src="assets/img/flow/play.png" alt="play"/>
                 <div className="grid grid-cols-2">
                     <div className="grid col-span-2 h-28 bg-gradient-to-r from-violet-600/75 to-pink-500/100 p-2">
@@ -17,7 +17,7 @@ export default function RecentItem () {
                     </div>
                 </div>
             </div>
-            <p className="text-justify my-1 w-56">Avec Adele, Shawn Mendes, Joel Adams, Ayra Starr</p>
+            <p className="text-justify my-1 px-1 w-56">Avec Adele, Shawn Mendes, Joel Adams, Ayra Starr</p>
         </div>
     )
 }
