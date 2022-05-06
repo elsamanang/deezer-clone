7d6e412173b6d79f94046a4ab2d89d8b346d2773
elsa manang <manangelsa07@gmail.com>
2022-05-06 18:21:11 +0200

    add news component

---
+++ src/components/news.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import RecentPlayItem from "../tools/recent-play";
+
+export default class News extends Component {
+
+    render() {
+        return (
+            <div className="row m-2">
+                <div className="grid">
+                    <h4 className="link_title">
+                    Nouveautés pour vous
+                        <span className="i mx-1"><i class="fa-solid fa-angle-right"></i></span>
+                    </h4>
+                </div>
+                <div className="grid grid-cols-4 gap-8 my-2">
+                    <RecentPlayItem />
+                    <RecentPlayItem />
+                    <RecentPlayItem />
+                    <RecentPlayItem />
+                </div>
+            </div>
+        )
+    }
+}
